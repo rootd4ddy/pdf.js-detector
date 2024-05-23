27cdf684c97c31382ab5012e18354b2959e64213
rootd4ddy <129632649+rootd4ddy@users.noreply.github.com>
2024-05-23 10:48:23 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.sendMessage({ type: 'getAlertsEnabled' }, (response) => {
   if (response.alertsEnabled && document.querySelector('script[src*="pdf.js"]')) {
-    alert("pdf.js is being used on this webpage!");
+    alert("pdf.js detected!");
     chrome.runtime.sendMessage({ type: 'logEndpoint', endpoint: window.location.href });
   }
 });
